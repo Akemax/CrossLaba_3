6559e20bf6ce8523c53a5aff1bd48f0c191d640b
Akemax <silver.maks10@gmail.com>
2021-06-03 22:31:06 +0300

    Добавьте файлы проекта.

---
+++ App3/App3/Page.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public class Page : ContentPage
+    {
+        public Page()
+        {
+            Entry Entry1 = new Entry
+            {
+                Placeholder = "Write",
+                TextColor = Color.Brown
+            };
+
+            Button Button1 = new Button()
+            {
+                Text = "Tap"
+            };
+
+            Label Label1 = new Label()
+            {
+                Text = "Hello"
+            };
+
+            Button1.Clicked += (sender, args) => Label1.Text = Entry1.Text;
+
+            Content = new StackLayout
+            {
+                Children = { Entry1, Button1, Label1 }
+            };
+        }
+    }
+}
